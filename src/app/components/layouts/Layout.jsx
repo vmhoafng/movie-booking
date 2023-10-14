be3882c18b6d20fb51d19c5f46d997cc65792d6f
vmhoafng <vominhhoang044@gmail.com>
2023-10-14 22:24:36 +0700

    update resp

--- src/app/components/layouts/Layout.jsx
+++ src/app/components/layouts/Layout.jsx
@@ -1,26 +1,26 @@
-import React, { Suspense } from 'react';
-import DesktopNavbar from '../Navbar/DesktopNavbar';
-import MobileNavbar from '../Navbar/MobileNavbar';
-import DesktopFooter from '../Footer/DesktopFooter';
-import MobileFooter from '../Footer/MobileFooter';
-import { Outlet } from 'react-router-dom';
+import React, { Suspense } from "react";
+import DesktopNavbar from "../Navbar/DesktopNavbar";
+import MobileNavbar from "../Navbar/MobileNavbar";
+import DesktopFooter from "../Footer/DesktopFooter";
+import MobileFooter from "../Footer/MobileFooter";
+import { Outlet } from "react-router-dom";
 
 const loading = () => <div className=""></div>;
 
 function Layout() {
-	return (
-		<div className="h-full">
-			<DesktopNavbar />
-			<MobileNavbar />
-			<main className="h-full">
-				<Suspense fallback={loading()}>
-					<Outlet />
-				</Suspense>
-			</main>
-			<DesktopFooter />
-			<MobileFooter />
-		</div>
-	);
+  return (
+    <div className="h-full bg-bgPrimary ">
+      <DesktopNavbar />
+      <MobileNavbar />
+      <main className="h-full">
+        <Suspense fallback={loading()}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <DesktopFooter />
+      <MobileFooter />
+    </div>
+  );
 }
 
 export default Layout;
