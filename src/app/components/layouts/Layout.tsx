6f5297e1b7afc6caac1e0dbe229a90287256ab06
vmhoafng <vominhhoang044@gmail.com>
2023-10-17 18:31:40 +0700

    update components

--- src/app/components/layouts/Layout.tsx
+++ src/app/components/layouts/Layout.tsx
@@ -1,28 +1,39 @@
-import React, { Suspense } from 'react';
-import DesktopNavbar from '../Navbar/DesktopNavbar';
-import MobileNavbar from '../Navbar/MobileNavbar';
-import DesktopFooter from '../Footer/DesktopFooter';
-import MobileFooter from '../Footer/MobileFooter';
-import { Outlet } from 'react-router-dom';
+import React, { Suspense } from "react";
+import DesktopNavbar from "../Navbar/DesktopNavbar";
+import MobileNavbar from "../Navbar/MobileNavbar";
+import DesktopFooter from "../Footer/DesktopFooter";
+import MobileFooter from "../Footer/MobileFooter";
+import { Outlet } from "react-router-dom";
 
 const loading = () => <div className=""></div>;
 
-function Layout() {
-	return (
-		<div className="h-full w-full ">
-			{/* <DesktopNavbar /> */}
-			{/* <MobileNavbar /> */}
-			<main className="h-full">
-				<div className="container sm:mx-auto">
-					<Suspense fallback={loading()}>
-						<Outlet />
-					</Suspense>
-				</div>
-			</main>
-			{/* <DesktopFooter /> */}
-			{/* <MobileFooter /> */}
-		</div>
-	);
+type LayoutProps = {
+  backgroundImage?: string;
+};
+
+function Layout({ backgroundImage }: LayoutProps) {
+  return (
+    <div className="h-full w-full ">
+      {/* <DesktopNavbar /> */}
+      {/* <MobileNavbar /> */}
+      <div
+        className="object-cover"
+        style={{ backgroundImage: backgroundImage }}
+      >
+        <div className="bg-bgPrimary/80">
+          <main className="h-full">
+            <div className="container md:mx-auto">
+              <Suspense fallback={loading()}>
+                <Outlet />
+              </Suspense>
+            </div>
+          </main>
+        </div>
+      </div>
+      {/* <DesktopFooter /> */}
+      {/* <MobileFooter /> */}
+    </div>
+  );
 }
 
 export default Layout;
