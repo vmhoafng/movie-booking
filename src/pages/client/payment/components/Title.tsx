6911585e8ab676194f5c88a2534ffa48afc2f70e
vmhoafng <vominhhoang044@gmail.com>
2023-10-17 14:40:16 +0700

    update components

--- src/pages/client/payment/components/Title.tsx
+++ src/pages/client/payment/components/Title.tsx
@@ -7,15 +7,7 @@ interface TitleProps {
 function Title({ children }: TitleProps) {
   return (
     <h2 className="h-14 lg:h-16 py-2 text-white/90 font-semibold uppercase font-inter flex items-center">
-      <span
-        className="
-    leading-10
-  [text-shadow:0.5px_0.5px_1px_var(--tw-shadow-color)]
-  shadow-black/50
-  "
-      >
-        {children}
-      </span>
+      {children}
     </h2>
   );
 }
