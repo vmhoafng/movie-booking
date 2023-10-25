b2b09490efed98b1a1112ff0c01f0331a51a2849
Nolb226 <hn26677@gmail.com>
2023-10-25 20:49:01 +0700

    update

--- src/app/services/modules/movie.ts
+++ src/app/services/modules/movie.ts
@@ -1,19 +1,21 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 import { ENDPOINTS } from '@/app/constants/endpoint';
+import { IGetListParams } from '@/app/types/common/api';
 import { IMovieByStatusList } from '@/app/types/movie';
 import { Axios } from '@/app/utils/api';
 import { AxiosResponse } from 'axios';
 
 export default {
 	getMoviesByStatus: async (
-		status: string
+		status: string,
+		params?: IGetListParams
 	): Promise<AxiosResponse<IMovieByStatusList>> => {
 		return await Axios.axiosGet(ENDPOINTS.MOVIE_BY_STATUS, {
 			params: {
 				status: status || 'showing-now',
-				page: 1,
-				size: 10,
+				size: params?.size || 10,
+				page: params?.page || 1,
 			},
 		});
 	},
