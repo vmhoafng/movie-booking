4e9938e31d1aff089a4be89ea6ec4a48f78d4b23
vmhoafng <vominhhoang044@gmail.com>
2023-10-30 23:46:37 +0700

    update api

--- src/app/constants/endpoint.ts
+++ src/app/constants/endpoint.ts
@@ -1,25 +1,28 @@
-import { ROUTE_PARAMS } from './path';
+import { ROUTE_PARAMS } from "./path";
 
 export const ENDPOINTS = {
-	MOVIE_BY_STATUS: `landing/status/movies`,
-	GET_SHOWTIME_BY_CINEMA_DATE: `landing/cinema/${ROUTE_PARAMS.CINEMA_ID}/showtime`,
-	CINEMA_MOVIES_SHOWTIME: 'landing/cinema/movie/showtime',
-	SHOWTIME_BY_MOVIE: `lading/movie/${ROUTE_PARAMS.MOVIE_ID}/showtime`,
-	SEARCH_MOVIE: `landing/searchMovie`,
-	SHOWTIME_SEAT: `landing/showtime/${ROUTE_PARAMS.SHOWTIME_ID}/seats`,
-	MOVIE_GENRES: `landing/movieGenres`,
-	CINEMA_DETAILS: `landing/cinema/${ROUTE_PARAMS.CINEMA_ID}`,
-	CINEMA_LIST: `landing/cinemas`,
-	PROFILE: 'currentUser',
-	ADMIN: {},
+  MOVIE_BY_STATUS: `landing/status/movies`,
+  GET_SHOWTIME_BY_CINEMA_DATE: `landing/cinema/${ROUTE_PARAMS.CINEMA_ID}/showtime`,
+  CINEMA_MOVIES_SHOWTIME: "landing/cinema/movie/showtime",
+  SHOWTIME_BY_MOVIE: `lading/movie/${ROUTE_PARAMS.MOVIE_ID}/showtime`,
+  SEARCH_MOVIE: `landing/searchMovie`,
+  SHOWTIME_SEAT: `landing/showtime/${ROUTE_PARAMS.SHOWTIME_ID}/seats`,
+  MOVIE_GENRES: `landing/movieGenres`,
+  CINEMA_DETAILS: `landing/cinema/${ROUTE_PARAMS.CINEMA_ID}`,
+  CINEMA_LIST: `landing/cinemas`,
+  PROFILE: {
+    DATA: "profile",
+    CHECKPASSWORD: "checkPassword",
+  },
+  ADMIN: {},
 };
 
 export const getEndPoint = (endpoint: string, arg: Object) => {
-	let newEndpoint = '';
-	Object.keys(arg).forEach((key) => {
-		const regex = new RegExp('(:' + key + ')', 'g');
-		//@ts-ignore
-		newEndpoint = endpoint.replace(regex, arg[key]);
-	});
-	return newEndpoint;
+  let newEndpoint = "";
+  Object.keys(arg).forEach((key) => {
+    const regex = new RegExp("(:" + key + ")", "g");
+    //@ts-ignore
+    newEndpoint = endpoint.replace(regex, arg[key]);
+  });
+  return newEndpoint;
 };
