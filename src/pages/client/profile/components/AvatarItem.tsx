90a83450d3c704a9f7b1043cce93f33cb5e225bf
vmhoafng <vominhhoang044@gmail.com>
2023-11-02 21:30:52 +0700

    hot fix

--- src/pages/client/profile/components/AvatarItem.tsx
+++ src/pages/client/profile/components/AvatarItem.tsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import AvatarModal from "./AvatarModal";
+import Icon from "@/app/components/icon/Icon";
 
 function AvatarItem() {
+  const [openModal, setOpenModal] = useState(false);
   return (
     <div className="flex flex-col gap-[10px] items-center">
-      <div className="absolute bg-bgPrimary/20 w-screen h-screen"></div>
-      <img
-        src="/assets/images/bg-01.jpg"
-        alt=""
-        className="h-[150px] lg:h-[120px] 2xl:h-[150px] w-[150px] lg:w-[120px] 2xl:w-[150px] border-[3px] rounded-full object-cover border-borderColor "
-      />
+      {openModal && (
+        <AvatarModal
+          onClick={() => {
+            setOpenModal(false);
+          }}
+        />
+      )}
+      <div className="relative">
+        <img
+          onClick={() => {
+            setOpenModal(true);
+          }}
+          src="/assets/images/bg-01.jpg"
+          alt=""
+          className="
+            h-[150px]
+            lg:h-[120px]
+            2xl:h-[150px]
+            w-[150px]
+            lg:w-[120px]
+            2xl:w-[150px]
+            border-[3px]
+            rounded-full
+            object-cover
+            border-borderColor"
+        />
+        <div className="absolute right-0 bottom-4 lg:bottom-1 2xl:bottom-4">
+          <Icon width={30} height={30} icon="smile" />
+        </div>
+      </div>
       <div className="flex flex-col items-center">
         <div className="text-[15px] text-white font-bold leading-6">
           Điểm thành viên
