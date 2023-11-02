c23d2af9c7c285beeba15ff6872969c38971062b
vmhoafng <vominhhoang044@gmail.com>
2023-11-02 14:08:57 +0700

    hot fix

--- src/app/components/Title.tsx
+++ src/app/components/Title.tsx
@@ -2,24 +2,26 @@ import React from "react";
 import clsx from "clsx";
 
 interface TitleProps {
-   children: React.ReactNode;
-   active?: Boolean;
-   onClick?: () => void;
+  children: React.ReactNode;
+  active?: Boolean;
+
+  onClick?: () => void;
 }
 
 function Title({ children, active, onClick }: TitleProps) {
-   return (
-      <div onClick={onClick} className=" cursor-pointer">
-         <h2
-            className={clsx(
-               `uppercase text-base font-bold text-white border-b p-[2px] hover:border-highlight transition-all duration-300`,
-               active ? "border-highlight w-fit" : "border-transparent"
-            )}
-         >
-            {children}
-         </h2>
-      </div>
-   );
+  return (
+    <div onClick={onClick} className=" cursor-pointer">
+      <h2
+        className={clsx(
+          `uppercase text-base font-bold text-white border-b p-[2px] transition-all duration-300`,
+          active ? "border-highlight w-fit" : "border-transparent",
+          onClick && "hover:border-highlight"
+        )}
+      >
+        {children}
+      </h2>
+    </div>
+  );
 }
 
 export default Title;
