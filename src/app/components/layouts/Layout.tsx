71a405ec211d6efc9db2a9c82323ae8694dd0ff8
Nolb226 <hn26677@gmail.com>
2023-11-10 01:14:04 +0700

    fix-ui

--- src/app/components/layouts/Layout.tsx
+++ src/app/components/layouts/Layout.tsx
@@ -1,44 +1,45 @@
-import React, { Suspense } from "react";
-import DesktopNavbar from "../Navbar/DesktopNavbar";
-import MobileNavbar from "../Navbar/MobileNavbar";
-import DesktopFooter from "../Footer/DesktopFooter";
-import MobileFooter from "../Footer/MobileFooter";
-import { Outlet } from "react-router-dom";
-import LoadingAnimation from "../loading/LoadingAnimation";
+import React, { Suspense } from 'react';
+import DesktopNavbar from '../Navbar/DesktopNavbar';
+import MobileNavbar from '../Navbar/MobileNavbar';
+import DesktopFooter from '../Footer/DesktopFooter';
+import MobileFooter from '../Footer/MobileFooter';
+import { Outlet } from 'react-router-dom';
+import LoadingAnimation from '../loading/LoadingAnimation';
+import HeroSection from './HeroSection';
 
 const loading = () => <LoadingAnimation />;
 
 type LayoutProps = {
-   backgroundImage?: string;
+	backgroundImage?: string;
 };
 
 function Layout({ backgroundImage }: LayoutProps) {
-   return (
-      <div className="h-full w-full">
-         <DesktopNavbar />
-         {/* <MobileNavbar /> */}
-         <div
-            className="object-cover mt-[146px]"
-            style={{
-               backgroundImage: `url('/assets/images/${backgroundImage}')`,
-            }}
-         >
-            <div
-               className={backgroundImage ? "bg-bgPrimary/80" : "bg-bgPrimary"}
-            >
-               <main className="h-full">
-                  <div className="container px-[15px] md:px-0 md:mx-auto">
-                     <Suspense fallback={loading()}>
-                        <Outlet />
-                     </Suspense>
-                  </div>
-               </main>
-            </div>
-         </div>
-         <DesktopFooter />
-         {/* <MobileFooter /> */}
-      </div>
-   );
+	return (
+		<div className="h-full w-full relative">
+			<DesktopNavbar />
+			{/* <MobileNavbar /> */}
+			<div
+				className="object-cover md:mt-[146px] mt-[96px]"
+				style={{
+					backgroundImage: `url('/assets/images/${backgroundImage}')`,
+				}}
+			>
+				<HeroSection />
+
+				<div className={backgroundImage ? 'bg-bgPrimary/80' : 'bg-bgPrimary'}>
+					<main className="h-full">
+						<div className="container px-[15px] md:px-0 md:mx-auto">
+							<Suspense fallback={loading()}>
+								<Outlet />
+							</Suspense>
+						</div>
+					</main>
+				</div>
+			</div>
+			<DesktopFooter />
+			{/* <MobileFooter /> */}
+		</div>
+	);
 }
 
 export default Layout;
