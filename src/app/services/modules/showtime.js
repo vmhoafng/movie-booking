d219d17c7ac73fb2e091b4ab1094718262b1945e
Nolb226 <hn26677@gmail.com>
2023-11-15 13:31:58 +0700

    fix API

--- src/app/services/modules/showtime.js
+++ src/app/services/modules/showtime.js
@@ -1,27 +1,32 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { ENDPOINTS, getEndPoint } from "@/app/constants/endpoint";
-import { Axios } from "@/app/utils/api";
-import axios from "axios";
+import { ENDPOINTS, getEndPoint } from '@/app/constants/endpoint';
+import { Axios } from '@/app/utils/api';
+import axios from 'axios';
 
 export default {
-  getShowtimeByMovie: (id, date) => {
-    return Axios.axiosGet(
-      getEndPoint(ENDPOINTS.SHOWTIME_BY_MOVIE, { movieId: id }),
-      {
-        params: {
-          date,
-        },
-      }
-    );
-  },
-  getShowtimeByCinema: (id, date) => {
-    return axios.get(
-      `${process.env.REACT_APP_BOOKING_MOVIE_API_URL}/landing/cinema/${id}/showtime?${date}`
-    );
-  },
-  getSeatsByShowtime: (id) => {
-    return axios.get(
-      `${process.env.REACT_APP_BOOKING_MOVIE_API_URL}/landing/showtime/${id}/seats`
-    );
-  },
+	getShowtimeByMovie: (id, date) => {
+		return Axios.axiosGet(
+			getEndPoint(ENDPOINTS.SHOWTIME_BY_MOVIE, { movieId: id }),
+			{
+				params: {
+					date,
+				},
+			}
+		);
+	},
+	getShowtimeByCinema: (id, date) => {
+		return axios.get(
+			`${process.env.REACT_APP_BOOKING_MOVIE_API_URL}/landing/cinema/${id}/showtime?${date}`
+		);
+	},
+	getSeatsByShowtime: (id) => {
+		// return axios.get(
+		//   `${process.env.REACT_APP_BOOKING_MOVIE_API_URL}/landing/showtime/${id}/seats`
+		// );
+		return Axios.axiosGet(
+			getEndPoint(ENDPOINTS.SHOWTIME_SEAT, {
+				showTimeId: id,
+			})
+		);
+	},
 };
