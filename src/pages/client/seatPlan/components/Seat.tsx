42d43d79e9b712b72d1ef34711bcf184b42b49f0
ThanhDat653 <datnt.653@gmail.com>
2023-11-25 16:45:38 +0700

    update seat selected

--- src/pages/client/seatPlan/components/Seat.tsx
+++ src/pages/client/seatPlan/components/Seat.tsx
@@ -4,21 +4,31 @@ import { ISeatType } from "../type";
 import { useRedux } from "@/app/hooks";
 import { setSelectedSeats } from "@/app/redux/payment";
 
-const Seat = ({ seat, is_reserved }: { seat: ISeatType, is_reserved: boolean }) => {
-   const { dispatch, } = useRedux();
-
+const Seat = ({
+   seat,
+   is_reserved,
+}: {
+   seat: ISeatType;
+   is_reserved: boolean;
+}) => {
+   const { dispatch } = useRedux();
 
    return (
       <button
          className={clsx(
-            "flex justify-center items-center h-8 w-8 border-2 border-bgPrimary rounded-lg text-center hover:bg-highlight cursor-pointer transition-colors duration-150",
-            seat?.status ? "bg-borderColor" : "bg-gradientStart hover:none",
+            "relactive flex justify-center items-center h-8 w-8 border-2 border-bgPrimary rounded-lg text-center  cursor-pointer transition-colors duration-150",
+            !seat?.is_reserved
+               ? "bg-borderColor hover:bg-highlight"
+               : "bg-gradientStart select-none",
             is_reserved && "bg-highlight"
          )}
-         disabled={!seat?.status}
+         disabled={seat?.is_reserved}
          onClick={() => dispatch(setSelectedSeats(seat))}
       >
          <span className="text-sm">{seat?.row_index as any}</span>
+         {/* {seat?.is_reserved === true && (
+            <div className="absolute top-0 left-0 w-full h-full"></div>
+         )} */}
       </button>
    );
 };
