76fcbac58333cbc7a48ff963f012d33717177ead
vmhoafng <vominhhoang044@gmail.com>
2023-12-01 09:58:11 +0700

    update

--- src/app/services/modules/movies/movies.service.ts
+++ src/app/services/modules/movies/movies.service.ts
@@ -1,63 +1,71 @@
-import { ENDPOINTS, getEndPoint } from '@/app/constants/endpoint';
+import { ENDPOINTS, getEndPoint } from "@/app/constants/endpoint";
 import {
-	IMovieSlug,
-	IPutMovieDetails,
-	IgetByStatus,
-	IgetShowtimeByMovie,
-} from '@/app/types/movie';
-import { Axios } from '@/app/utils/api';
+  IMovieSlug,
+  IPutMovieDetails,
+  IgetByStatus,
+  IgetShowtimeByMovie,
+} from "@/app/types/movie";
+import { Axios } from "@/app/utils/api";
 const moviesService = {
-	getAll: async (payload: IgetByStatus) => {
-		return await Axios.axiosGet(ENDPOINTS.MOVIE_BY_STATUS, {
-			params: {
-				status: payload.status || 'showing-now',
-				page: payload.page || 1,
-				size: payload.size || 100,
-			},
-		});
-	},
-	getMovieSlug: (payload: IMovieSlug) => {
-		return Axios.axiosGet(
-			getEndPoint(ENDPOINTS.MOVIE_SLUG, {
-				movieSlug: payload.slug,
-			})
-		);
-	},
+  getAll: async (payload: IgetByStatus) => {
+    return await Axios.axiosGet(ENDPOINTS.MOVIE_BY_STATUS, {
+      params: {
+        status: payload.status || "showing-now",
+        page: payload.page || 1,
+        size: payload.size || 100,
+      },
+    });
+  },
+  getMovieSlug: (payload: IMovieSlug) => {
+    return Axios.axiosGet(
+      getEndPoint(ENDPOINTS.MOVIE_SLUG, {
+        movieSlug: payload.slug,
+      })
+    );
+  },
 
-	getMovieById: (id: string) => {
-		return Axios.axiosGetWithToken(
-			getEndPoint(ENDPOINTS.ADMIN.MOVIE.DETAIL, {
-				movieId: id,
-			})
-		);
-	},
+  getMovieById: (id: string) => {
+    return Axios.axiosGetWithToken(
+      getEndPoint(ENDPOINTS.ADMIN.MOVIE.DETAIL, {
+        movieId: id,
+      })
+    );
+  },
 
-	getMovieShowtimes: (payload: IgetShowtimeByMovie) => {
-		const { id, date } = payload;
-		return Axios.axiosGet(
-			getEndPoint(ENDPOINTS.SHOWTIME_BY_MOVIE, {
-				movieId: id,
-			}),
-			{
-				params: {
-					date,
-				},
-			}
-		);
-	},
+  getMovieShowtimes: (payload: IgetShowtimeByMovie) => {
+    const { id, date } = payload;
+    return Axios.axiosGet(
+      getEndPoint(ENDPOINTS.SHOWTIME_BY_MOVIE, {
+        movieId: id,
+      }),
+      {
+        params: {
+          date,
+        },
+      }
+    );
+  },
 
-	putMovie: (id: string, payload: IPutMovieDetails) => {
-		return Axios.axiosPutWithFile(
-			getEndPoint(ENDPOINTS.ADMIN.MOVIE.PUT, {
-				movieId: id,
-			}),
-			payload
-		);
-	},
+  putMovie: (id: string, payload: IPutMovieDetails) => {
+    return Axios.axiosPutWithFile(
+      getEndPoint(ENDPOINTS.ADMIN.MOVIE.PUT, {
+        movieId: id,
+      }),
+      payload
+    );
+  },
 
-	getAllMovies: async () => {
-		return Axios.axiosGet(ENDPOINTS.MOVIES);
-	},
+  getAllMovies: async () => {
+    return Axios.axiosGet(ENDPOINTS.MOVIES);
+  },
+  getAllAdminMovies: async () => {
+    return Axios.axiosGetWithToken(ENDPOINTS.ADMIN.MOVIE.LIST, {
+      params: {
+        size: 9999,
+        page: 1,
+      },
+    });
+  },
 };
 
 export default moviesService;
