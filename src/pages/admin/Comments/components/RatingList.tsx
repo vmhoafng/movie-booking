cc04ca4947bdfbdbbf46e67ddd63228a6de29444
Nolb226 <hn26677@gmail.com>
2023-12-07 22:43:43 +0700

    fix UI

--- src/pages/admin/Comments/components/RatingList.tsx
+++ src/pages/admin/Comments/components/RatingList.tsx
@@ -1,25 +1,23 @@
-import React from "react";
-import Rating from "./Rating";
-import { IComment } from "@/app/types/comment";
+import React from 'react';
+import Rating from './Rating';
+import { IComment } from '@/app/types/comment';
 interface RatingListProps {
-   data: IComment[];
-   currentPage: number;
-   itemsPerPage: number;
+    data: IComment[];
+    currentPage: number;
+    itemsPerPage: number;
 }
 
 const RatingList = ({ data, currentPage, itemsPerPage }: RatingListProps) => {
-   console.log(data);
-
-   const startIndex = (currentPage - 1) * itemsPerPage;
-   const endIndex = startIndex + itemsPerPage;
-   const displayedData = data?.slice(startIndex, endIndex);
-   return (
-      <>
-         {displayedData?.map((comment) => {
-            return <Rating data={comment} key={comment.id}></Rating>;
-         })}
-      </>
-   );
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const displayedData = data?.slice(startIndex, endIndex);
+    return (
+        <>
+            {displayedData?.map((comment) => {
+                return <Rating data={comment} key={comment.id}></Rating>;
+            })}
+        </>
+    );
 };
 
 export default RatingList;
